dea1897f422c5af24c0bbe1eded5da2e79108450
Jorge Peña <jandres-0818@hotmail.com>
2023-04-24 02:23:15 -0500

    Configuracion de ambiente

---
+++ src/test/java/com/eduocio/EduyOcioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eduocio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EduyOcioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
